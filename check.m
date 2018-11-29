b7bf2a80fa31e229c2f1c2e31eabbc68f33bc5c9
wernerolaf <olafowerner@gmail.com>
2018-11-29 16:22:40 +0100

    chyba dziala

--- check.m
+++ check.m
@@ -1,10 +1,11 @@
-function [results] = check(mult, max_iter, xs)
+function [results] = check(mult, max_iter,keps)
 canon=mult_to_canon(mult);
 results=zeros(size(mult));
 j=size(canon);
+xs=nodes(canon);
 for i=1:j(1)
-    x=weierstrass(canon(i,:),max_iter,xs);
-    results(i,:)=x(:,end)'-mult(i,:);
+    x=weierstrass(canon(i,:),max_iter,xs(i,:),keps);
+    results(i,:)=x(:,end)';%-mult(i,:);
 end
 return
 end
