5b82c030425955419bf081a5fb4c4bb1a7ae3750
wernerolaf <olafowerner@gmail.com>
2018-11-29 20:38:20 +0100

    wykres

--- testy.m
+++ testy.m
@@ -1,7 +1,15 @@
-mult=[1 5;4 6];
+mult=[2 5;4 6];
 xs=[3 5];
-max_iter=10;
+max_iter=20;
 canon=mult_to_canon(mult)
-x=weierstrass([5 -6 1],max_iter,xs,4)
+
     
-nodes
+
+
+x=-5:.1:5;
+p=[1 0 -1]; % polynomial function
+plot(x,polyval(p,x))
+
+y=weierstrass(p,max_iter,nodes(p),4);
+plot(x,polyval(p,x),y(1,:),polyval(p,y(1,:)))
+check(roots(p),30,1)
