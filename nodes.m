61b2dadd51e7346cc3b4538056f3a671f7977fed
wernerolaf <olafowerner@gmail.com>
2018-12-01 14:27:02 +0100

    zmiana nazwy

--- nodes.m
+++ nodes.m
@@ -1,8 +1,8 @@
-function [argstart] = nodes(canon)
-    j=size(canon);
+function [argstart] = nodes(coeff)
+    j=size(coeff);
     argstart=zeros(j(1),j(2)-1);
     for i=1:j(1)
-    x=sum(abs(canon(i,:)))+1;
+    x=sum(abs(coeff(i,:)))+1;
     argstart(i,:)=linspace(complex(x*-1,x*-1),complex(x,x),j(2)-1);
     end
     
