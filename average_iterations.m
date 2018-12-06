2300de73e8313da6bdb193bd5b328bf5840e8c06
k-sap <karlo975@hotmail.com>
2018-12-06 17:24:44 +0100

    dodatki

---
+++ average_iterations.m
@@ -0,0 +1,17 @@
+averages = zeros(1, 10);
+keps = 10;
+%stopien wielomianu
+for j=10:10:100
+average = 0;
+for i = 1:100
+%deg = randi([2, j], 1);
+deg = j;
+coeff = -25 + 50*rand(1, deg);
+
+iterations = weierstrass(coeff, 250, nodes(coeff), keps);
+average = average + convergence_raw(iterations, keps);
+end
+
+averages(j/10) = average/100;
+end
+plot(10:10:100, averages)
---
+++ repeated_roots.m
@@ -0,0 +1,4 @@
+coeff = [1, 2, -3, -4, 4]
+analytic_roots = [1, 1, -2, -2]
+%w = (x-1)^2*(x+2)^2
+weierstrass_roots(coeff)
